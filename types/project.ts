d4a5b0e575550494b27248743be2432a79c8e9fb
Jake <jgaunt200@gmail.com>
2026-04-11 14:16:04 -0700

    feat: add Project and ProjectFile TypeScript types

---
+++ types/project.ts
@@ -0,0 +1,27 @@
+import type { GuideCardData } from './guide'
+
+export interface ProjectFile {
+  id: string
+  projectId: string
+  name: string
+  size: number
+  mimeType: string
+  storageKey: string
+  uploadedAt: string
+}
+
+export interface ProjectCardData {
+  id: string
+  name: string
+  createdAt: string
+  fileCount: number
+  guideCount: number
+}
+
+export interface ProjectDetail {
+  id: string
+  name: string
+  createdAt: string
+  files: ProjectFile[]
+  guides: GuideCardData[]
+}
